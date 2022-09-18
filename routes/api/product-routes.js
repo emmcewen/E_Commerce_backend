96a33e74801b52ac7e804bc6aa6c74b296641d97
EMILY MCEWEN <emmcewen@gmail.com>
2022-09-18 16:17:01 -0400

    removing some comments to see if files are pushing to repository

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -74,14 +74,7 @@ router.post('/', (req, res) => {
     console.log(err);
     res.status(400).json(err);
   });
-  /* req.body should look like this...
-    {
-      "product_name": "Basketball",
-      "price": 200.00,
-      "stock": 3,
-      "tagIds": [1, 2, 3, 4]
-    }
-  */
+
 })
 // update product
 router.put('/:id', (req, res) => {
