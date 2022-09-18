009aba91ca858283be0f79c2ad7613ec5b29c791
EMILY MCEWEN <emmcewen@gmail.com>
2022-09-18 18:07:36 -0400

    trying to figure out why I can't get post to work

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo (Category,{
   foreignKey:"category_id"
-})
+});
 
 // Categories have many Products
 Category.hasMany(Product,{
@@ -17,13 +17,13 @@ Category.hasMany(Product,{
 Product.belongsToMany(Tag,{
   through:ProductTag,
   foreignKey:"product_id"
-})
+});
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product,{
   through:ProductTag,
   foreignKey:"tag_id"
-})
+});
 
 module.exports = {
   Product,
