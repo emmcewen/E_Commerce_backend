32a4451ea1d55cab78d6af293b62f4c44b755f3a
EMILY MCEWEN <emmcewen@gmail.com>
2022-09-22 22:20:18 -0400

    added delete route to code to get product routes to delete

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -118,7 +118,16 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((products) => res.json(products))
+    .catch((err) => {
+     console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 module.exports = router;
